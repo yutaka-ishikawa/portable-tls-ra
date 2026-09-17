b45abb6ee54ca87bee4a63dc459dd94b395d9305
Yutaka Ishikawa <yutaka_ishikawa@me.com>
2026-09-17 10:55:55 +0900

    snapshot

--- test_toctou/ex1.c
+++ test_toctou/ex1.c
@@ -10,15 +10,17 @@ main()
     printf("#Please issues the following commands from another window:\n");
     printf("$ ls -l /proc/%d/exe\n", pid);
     printf("$ stat /proc/%d/exe\n", pid);
+    printf("$ sha256sum /proc/%d/exe\n", pid);
     printf("$ cp ex2 ex1\n");
-    printf("# this copy fails because the file is busy\nThen,\n");
-    printf("$ mv ex1 ex1.save\n");
+    printf("# this copy fails because the file is busy. But remove is allowed:\n");
+    printf("$ rm ex1\n");
     printf("$ cp ex2 ex1\n");
     printf("# Look at the exe path!!\n");
     printf("$ ls -l /proc/%d/exe\n", pid);
     printf("$ stat /proc/%d/exe\n", pid);
-    printf("You will see the exe path is changed to ex1.save\n");
-    printf("That is, TOCTOU attacks on binary measurement are prevented by using the /proc/<pid>/exe symlink.\n");
+    printf("$ sha256sum /proc/%d/exe\n", pid);
+    printf("You will see the exe path still follows the original ex1 binary. The same hash value is obtained.\n");
+    printf("That is, TOCTOU attacks on binary measurement are prevented by measuring via the /proc/<pid>/exe symlink.\n");
     
     fgets(buf, sizeof(buf), stdin);
     return 0;
